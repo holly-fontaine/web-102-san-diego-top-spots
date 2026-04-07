321c3aa23beab202e768e2b6fb6e3786067fcd28
holly-fontaine <hollybf@proton.me>
2026-04-07 01:59:27 +0100

    san-diego-top-spots-finished

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
-$(document).ready(function () {
-    // write your code here
+$.getJSON("data.json", (data) => {
+    $.each(data, (index, spot) => {
+        $("#spotsTable").append(
+
+            `<tr>
+                <td>${spot.name}</td>
+                <td>${spot.description}</td>
+                <td>
+                    <a href= "https://www.google.com/maps?q=${spot.location}" target = "_blank"> Google Maps
+                </td>
+            </tr>`
+        );
+    }
+    );
 
 });
